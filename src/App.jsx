c4efa8103c7edf27e7c7ea53ef6262a4d1ed2044
Juan David Franco <jfrancoh5@gmail.com>
2024-04-08 17:40:09 -0500

    first component header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <>
+     <main className='contenedor'>
+        <Header/>
+      </main> 
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import '../../styles/components/header.css'
+
+const Header = () =>{
+    const [burguer,changeBurguer] = useState(true);
+
+    const toggleBurguer = () =>{
+        changeBurguer(!burguer);
+    }
+
+    return(
+        <header className="header">
+            <section className="header-burguer-button" onClick={toggleBurguer}>
+                <article className={burguer?'first-button-line':'first-button-line-change'}></article>
+                <article className={burguer?"second-button-line":"second-button-line-change"}></article>
+                <article className={burguer?"third-button-line":"third-button-line-change"}></article>
+            </section>
+            <label className="navbar-searcher">    
+                <input type="text" className="navbar-text"/>
+                <button className="navbar-loop">
+                    <span></span>
+                </button>
+            </label>
+        </header>
+    )
+}
+
+export default Header;

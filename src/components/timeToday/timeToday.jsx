c3010141772a2ac7e71f03d81e6cc11a9bfd553d
Juan David Franco <jfrancoh5@gmail.com>
2024-04-10 22:59:48 -0500

    tablet media query

--- src/components/timeToday/timeToday.jsx
+++ src/components/timeToday/timeToday.jsx
@@ -5,6 +5,7 @@ import maxmin from '../../assets/icons/max-min.svg';
 import humidity from '../../assets/icons/humidity.svg';
 import pressure from '../../assets/icons/pressure.svg';
 import wind from '../../assets/icons/wind.svg';
+import visibility from '../../assets/icons/wind.svg'
 import pointrose from '../../assets/icons/point-rose.svg';
 
 const TimeToday = () =>{
@@ -27,6 +28,9 @@ const TimeToday = () =>{
                 <img src={pressure}/>
                 <p>Presión</p>
                 <p>1029,1mb</p>
+                <img src={visibility}/>
+                <p>Visibilidad</p>
+                <p>9.66 km</p>
                 <img src={wind}/>
                 <p>Viento</p>
                 <p>23 km/h</p>
